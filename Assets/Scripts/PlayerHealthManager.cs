e96d95a42b12d10742c7e72dfe7c168f735f4421
weirdcloud <63076259+weirdcloud@users.noreply.github.com>
2021-10-17 05:32:59 +0300

    Player health manager

---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 3;
+    [SerializeField]
+    private int currentHealth;
+
+    public UnityEvent<int, int> OnChangeHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHealth(GameObject dealer, int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        OnChangeHealth.Invoke(maxHealth, currentHealth);
+        if (currentHealth == 0)
+        {
+            Die(dealer);
+        }
+    }
+    public void Die(GameObject killer)
+    {
+        Debug.Log(gameObject.name + " was killed by " + killer.name);
+        Destroy(gameObject);
+    }
+}
